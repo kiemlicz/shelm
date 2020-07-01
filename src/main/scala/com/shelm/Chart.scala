b1b97413b3d7d5b2abe13791ad9931d2a1e2587d
stanislaw_jakiel <stanislaw.dev@gmail.com>
2020-07-01 23:07:57 +0200

    WIP SBT Helm plugin yaml decoders

--- src/main/scala/com/shelm/Chart.scala
+++ src/main/scala/com/shelm/Chart.scala
@@ -1,45 +1,138 @@
 package com.shelm
 
+import java.io.FileReader
 import java.net.URI
 
-import io.circe.{Decoder, Encoder}
+import io.circe.{Decoder, DecodingFailure, HCursor, yaml}
 
 case class Chart(apiVersion: String,
                  name: String,
                  version: String,
                  kubeVersion: Option[String],
                  description: Option[String],
-                 `type`: Option[String],
-                 keywords: List[String],
-                 home: Option[String],
-                 sources: List[URI],
-                 dependencies: List[ChartDependency],
-                 maintainers: List[ChartMaintainer],
+                 tpe: Option[ChartType],
+                 keywords: Option[List[String]],
+                 home: Option[URI],
+                 sources: Option[List[URI]],
+                 dependencies: Option[List[ChartDependency]],
+                 maintainers: Option[List[ChartMaintainer]],
                  icon: Option[URI],
                  appVersion: Option[String],
                  deprecated: Option[Boolean],
-                 annotations: ChartAnnotations)
+                 annotations: Option[ChartAnnotations])
 object Chart {
-  implicit val decoder: Decoder[Chart] = ???
+  implicit val decoder: Decoder[Chart] = (c: HCursor) =>
+    for {
+      apiVersion <- c.get[String]("apiVersion")
+      name <- c.get[String]("name")
+      version <- c.get[String]("version")
+      kubeVersion <- c.get[Option[String]]("kubeVersion")
+      description <- c.get[Option[String]]("description")
+      tpe <- c.get[Option[ChartType]]("type")
+      keywords <- c.get[Option[List[String]]]("keywords")
+      home <- c.get[Option[String]]("home").map(_.map(URI.create))
+      sources <- c
+        .get[Option[List[String]]]("sources")
+        .map(_.map(_.map(URI.create)))
+      dependencies <- c.get[Option[List[ChartDependency]]]("dependencies")
+      maintainers <- c.get[Option[List[ChartMaintainer]]]("maintainers")
+      icon <- c.get[Option[String]]("icon").map(_.map(URI.create))
+      appVersion <- c.get[Option[String]]("appVersion")
+      deprecated <- c.get[Option[Boolean]]("deprecated")
+      annotations <- c.get[Option[ChartAnnotations]]("annotations")
+    } yield
+      Chart(
+        apiVersion,
+        name,
+        version,
+        kubeVersion,
+        description,
+        tpe,
+        keywords,
+        home,
+        sources,
+        dependencies,
+        maintainers,
+        icon,
+        appVersion,
+        deprecated,
+        annotations
+    )
 }
 
-case class ChartDependency()
+object Main {
+  def main(args: Array[String]): Unit = {
+    println(
+      yaml.parser
+        .parse(
+          new FileReader("src/sbt-test/shelm/simple/simple-chart/Chart.yaml")
+        )
+        .flatMap(_.as[Chart])
+    )
+  }
+}
+//fixme further decoders
+case class ChartDependency(name: String,
+                           version: String,
+                           repository: URI,
+                           condition: Option[String],
+                           tags: Option[List[String]],
+                           enabled: Option[Boolean],
+                           importValues: Option[List[String]],
+                           alias: Option[String])
 object ChartDependency {
-  implicit val decoder: Decoder[ChartDependency] = ???
-}
-case class ChartMaintainer()
-object ChartMaintainer {
-  implicit val decoder: Decoder[ChartMaintainer] = ???
-}
-case class ChartAnnotations()
-object ChartAnnotations {
-  implicit val decoder: Decoder[ChartAnnotations] = ???
+  implicit val decoder: Decoder[ChartDependency] = (c: HCursor) =>
+    for {
+      name <- c.get[String]("name")
+      version <- c.get[String]("version")
+      repository <- c.get[String]("repository").map(URI.create)
+      condition <- c.get[Option[String]]("condition")
+      tags <- c.get[Option[List[String]]]("condition")
+      enabled <- c.get[Option[Boolean]]("enabled")
+      importValues <- c.get[Option[List[String]]]("import-values")
+      alias <- c.get[Option[String]]("alias")
+    } yield
+      ChartDependency(
+        name,
+        version,
+        repository,
+        condition,
+        tags,
+        enabled,
+        importValues,
+        alias
+    )
 }
 
-sealed abstract class ChartType(`type`: String)
+case class ChartMaintainer(name: String,
+                           email: Option[String],
+                           url: Option[URI])
+object ChartMaintainer {
+  implicit val decoder: Decoder[ChartMaintainer] = (c: HCursor) =>
+    for {
+      name <- c.get[String]("name")
+      email <- c.get[Option[String]]("email")
+      url <- c.get[Option[String]]("url").map(r => r.map(URI.create))
+    } yield ChartMaintainer(name, email, url)
+}
+
+case class ChartAnnotations(annotations: Map[String, String])
+object ChartAnnotations {
+  implicit val decoder: Decoder[ChartAnnotations] =
+    Decoder.decodeMap[String, String].map(ChartAnnotations(_))
+}
+
+sealed abstract class ChartType(val tpe: String)
 object ChartType {
   case object Application extends ChartType("application")
   case object Library extends ChartType("library")
 
-  implicit val decoder: Decoder[ChartType] = ???
+  private val caseObjects = Vector(Application, Library)
+
+  implicit val decoder: Decoder[ChartType] = (c: HCursor) =>
+    c.value.asString
+      .flatMap(s => caseObjects.find(_.tpe == s))
+      .toRight(
+        DecodingFailure(s"Wrong Chart type, must be one of $caseObjects", Nil)
+    )
 }
