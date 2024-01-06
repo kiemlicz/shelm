888ad9859f77c8ea9888b86437d3df9c58ac3216
stanislaw_jakiel <stanislaw.dev@gmail.com>
2024-01-06 14:14:20 +0100

    idea of batched publisher

---
+++ src/main/scala/io/github/kiemlicz/shelm/ChartPublisher.scala
@@ -0,0 +1,46 @@
+package io.github.kiemlicz.shelm
+
+import sbt.Artifact
+
+import java.io.{File, FileInputStream}
+import java.net.URI
+import java.net.http.HttpResponse.BodyHandlers
+import java.net.http.{HttpClient, HttpRequest}
+import java.util.concurrent.CompletableFuture
+
+object ChartPublisher {
+  def chartMuseumPublish(
+    httpClient: HttpClient,
+    uri: URI,
+    packagedArtifacts: Map[Artifact, File],
+    outstanding: Int //=1 but this will make overloading not work?
+  ): CompletableFuture[Unit] = {
+    //retry failed
+    packagedArtifacts.grouped(outstanding).foldLeft(CompletableFuture.supplyAsync(() => ())) { (acc, e) =>
+      acc.thenCompose(_ => chartMuseumPublish(httpClient, uri, e))
+    }
+  }
+
+  def chartMuseumPublish(
+    httpClient: HttpClient, uri: URI, packagedArtifacts: Map[Artifact, File]
+  ): CompletableFuture[Unit] = {
+    val ongoingPublish = for {
+      (artifact, artifactLocation) <- packagedArtifacts
+      r = HttpRequest.newBuilder()
+        .uri(uri)
+        .header("Content-Type", "application/octet-stream")
+        .POST(HttpRequest.BodyPublishers.ofInputStream(() => new FileInputStream(artifactLocation))
+        ) // configurable timeout handling?
+        .build()
+    } yield {
+      httpClient.sendAsync(r, BodyHandlers.ofString()) //fixme bodyhandler
+    }
+
+    CompletableFuture.allOf(ongoingPublish.toSeq *).thenApply { _ => // gather once the batch finishes
+      ongoingPublish.map(_.join())
+      ()
+    }: CompletableFuture[Unit]
+    //      .exceptionally(ex => null)
+    //      .join()
+  }
+}
