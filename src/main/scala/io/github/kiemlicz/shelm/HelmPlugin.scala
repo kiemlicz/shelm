e4bb452e27d35d975ecb8811f3dd3df4a447f878
stanislaw_jakiel <stanislaw.dev@gmail.com>
2025-12-15 17:30:37 +0100

    Support of Helm 4 More open check Added in lint too

--- src/main/scala/io/github/kiemlicz/shelm/HelmPlugin.scala
+++ src/main/scala/io/github/kiemlicz/shelm/HelmPlugin.scala
@@ -244,7 +244,7 @@ object HelmPlugin extends AutoPlugin {
     log: Logger,
   ): Unit = {
     val loginUri = helmVersion match {
-      case v@VersionNumber(Seq(major, minor, _@_*), _, _) if major == 4 || major == 3 && minor >= 8 => registry.loginArg(v).toString
+      case v@VersionNumber(Seq(major, minor, _@_*), _, _) if major > 3 || major == 3 && minor >= 8 => registry.loginArg(v).toString
       case _ => sys.error(s"Cannot login to OCI registry (Helm must be at least in 3.8.0 version): $helmVersion")
     }
 
@@ -323,7 +323,7 @@ object HelmPlugin extends AutoPlugin {
     log.info("Linting Helm Package")
     if (lintSettings.strictLint) {
       helmVersion match {
-        case VersionNumber(Seq(major, minor, _@_*), _, _) if major >= 3 && minor >= 14 =>
+        case VersionNumber(Seq(major, minor, _@_*), _, _) if major > 3 || major == 3 && minor >= 14 =>
         case _ => sys.error(s"Cannot perform helm lint --strict (Helm must be at least in 3.14.0 version): $helmVersion")
       }
     }
